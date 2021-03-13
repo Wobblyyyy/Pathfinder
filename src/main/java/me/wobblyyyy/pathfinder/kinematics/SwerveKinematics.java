42d61285f1ad37b03dc12100833f955e5c8a4c5c
colin robertson <30844830+Wobblyyyy@users.noreply.github.com>
2021-03-13 10:08:01 -0500

    Add a bit of documentation, easier to read and understand

--- src/main/java/me/wobblyyyy/pathfinder/kinematics/SwerveKinematics.java
+++ src/main/java/me/wobblyyyy/pathfinder/kinematics/SwerveKinematics.java
@@ -36,13 +36,51 @@ import me.wobblyyyy.pathfinder.geometry.Angle;
 import me.wobblyyyy.pathfinder.geometry.Point;
 import org.ejml.simple.SimpleMatrix;
 
+/**
+ * A variety of mathemtical functionality related to kinematics for a swerve
+ * chassis. Both forwarads and backwards (inverse) kinematic utilities are
+ * provided here, allowing for translation from desired translation to
+ * desired swerve module states as well as translation from desired swerve
+ * module states. The math behind this class is largely based upon the
+ * Efficient Java Matrix Library {@code SimpleMatrix} with a bit of fancy
+ * intervention here and there on our part. This class DOES NOT modify the
+ * drivetrain's states - this class simply provides a mathematical interface
+ * for determining module states and translations for a swerve chassis with
+ * a set of manually inputted wheel positions.
+ *
+ * @author Colin Robertson
+ * @since 0.3.0
+ */
 @SuppressWarnings("unused")
 public class SwerveKinematics {
+    /**
+     * Forwards kinematics - used for getting module states from a translation.
+     */
     private final SimpleMatrix kinematicsForwards;
+    
+    /**
+     *  Inverse kinematics - used for odometry purposes.
+     */
     private final SimpleMatrix kinematicsBackwards;
 
+    /**
+     * A container for each of the wheel positions on the robot.
+     */
     private final Arrayable<Point> wheelPositions;
 
+    /**
+     * Create a new {@code SwerveKinematics} instance. This class doesn't actually
+     * do anything other than provide a mathematical interface for kinematics.
+     *
+     * @param wheelPositions an {@code Arrayable} instance containing relative
+     *                       swerve module positions. This position is defined as
+     *                       the center axis on which the swerve module both drives
+     *                       and rotates. This array must be greater than two in
+     *                       size. Please note: the order of this array determines
+     *                       the order in which kinematic operations output power
+     *                       or positional values - remember the order you put these
+     *                       modules in, such as FR/FL/BR/BL.
+     */
     @SuppressWarnings("PointlessArithmeticExpression")
     public SwerveKinematics(final Arrayable<Point> wheelPositions) {
         if (wheelPositions.size() < 2) {
@@ -100,6 +138,20 @@ public class SwerveKinematics {
         return states;
     }
 
+    /**
+     * Get swerve module states based on the desired transformation and the
+     * wheel positions passed to this instance of {@code SwerveKinematics} on
+     * construction. These states are outputted in the SAME order as the wheels
+     * are inputted in upon construction. For example, if you created this instance
+     * of swerve kinematics with the order of FR/FL/BR/BL, the outputted states
+     * will be outputted in the same order.
+     *
+     * @param transform the robot's desired transformation. In most cases, this
+     *                  transformation should fit within the range of (-1, 1).
+     * @return a {@code StaticArray} of each of the module states that would represent
+     * the desired translation. These module states are returned in the same order as
+     * the wheel positions that were inputted upon this instance's construction.
+     */
     public StaticArray<SwerveModuleState> getStates(RTransform transform) {
         SimpleMatrix transformationVector = getTransformationVector(transform);
         SimpleMatrix moduleMatrix = getModuleMatrix(transformationVector);
@@ -131,6 +183,18 @@ public class SwerveKinematics {
         return kinematicsForwards.mult(stateMatrix);
     }
 
+    /**
+     * Get a positional transformation based on a set of swerve module states.
+     * If the size of the provided state array doesn't match the size of the
+     * internally-stored array of wheel positions, an exception will be thrown,
+     * indicating that no transformation could be generated as the size of the
+     * arrays did not match. 
+     *
+     * @param states a {@code StaticArray} of swerve module states. This array
+     *               should be the same size as the internal array of wheel
+     *               positions or an exception will be thrown on this method's
+     *               invocation. Not cool.
+     */
     public RTransform getTransform(StaticArray<SwerveModuleState> states) {
         if (states.size() != wheelPositions.size()) {
             throw new IllegalArgumentException(
