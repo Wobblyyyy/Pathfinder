7129a17f7e9a7b42426358629948e233c01fb7ab
colin <wobblyyyy@gmail.com>
2021-04-09 17:34:00 -0400

    tolerance

---
+++ src/main/java/me/wobblyyyy/pathfinder/math/Tolerance.java
@@ -0,0 +1,55 @@
+/*
+ * ======================================================================
+ * || Copyright (c) 2020 Colin Robertson (wobblyyyy@gmail.com)         ||
+ * ||                                                                  ||
+ * || This file is part of the "Pathfinder" project, which is licensed ||
+ * || and distributed under the GPU General Public License V3.         ||
+ * ||                                                                  ||
+ * || Pathfinder is available on GitHub:                               ||
+ * || https://github.com/Wobblyyyy/Pathfinder                          ||
+ * ||                                                                  ||
+ * || Pathfinder's license is available:                               ||
+ * || https://www.gnu.org/licenses/gpl-3.0.en.html                     ||
+ * ||                                                                  ||
+ * || Re-distribution of this, or any other files, is allowed so long  ||
+ * || as this same copyright notice is included and made evident.      ||
+ * ||                                                                  ||
+ * || Unless required by applicable law or agreed to in writing, any   ||
+ * || software distributed under the license is distributed on an "AS  ||
+ * || IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either  ||
+ * || express or implied. See the license for specific language        ||
+ * || governing permissions and limitations under the license.         ||
+ * ||                                                                  ||
+ * || Along with this file, you should have received a license file,   ||
+ * || containing a copy of the GNU General Public License V3. If you   ||
+ * || did not receive a copy of the license, you may find it online.   ||
+ * ======================================================================
+ *
+ */
+
+package me.wobblyyyy.pathfinder.math;
+
+public class Tolerance {
+    private final double tolerance;
+
+    public Tolerance(double tolerance) {
+        this.tolerance = tolerance;
+    }
+
+    public boolean compare(double reference,
+                           double target) {
+        double l = reference - tolerance;
+        double h = reference + tolerance;
+
+        return l <= target && target <= h;
+    }
+
+    public boolean compare(double reference,
+                           double[] targets) {
+        for (double target : targets) {
+            if (!compare(reference, target)) return false;
+        }
+
+        return true;
+    }
+}
